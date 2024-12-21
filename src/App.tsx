965edccb462d85a18a44db4e72cb6153090d85e7
Tom Winskell <129330818+tomwinskell@users.noreply.github.com>
2024-12-20 19:27:25 -0500

    Add button and input, tested connection with API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { css } from '@emotion/react';
+import { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const container = css({
+  display: 'flex',
+  gap: '1em',
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const padding = css({
+  borderRadius: '15px',
+  padding: '1rem',
+  fontSize: '1.5rem',
+});
+
+const button = css({
+  color: 'white',
+  background: 'navy',
+  border: 'none',
+});
+
+const input = css({
+  border: 'thin solid',
+});
+
+function handleClick(name: string) {
+  const obj = {
+    name,
+  };
+  console.log(obj);
+  axios
+    .post('http://localhost:3000/user', obj)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
 
-export default App
+export default function App() {
+  const [name, setName] = useState('');
+
+  return (
+    <div css={container}>
+      <input css={[input, padding]} onChange={(e) => setName(e.target.value)} />
+      <button css={[button, padding]} onClick={() => handleClick(name)}>
+        Register
+      </button>
+    </div>
+  );
+}
